48fa4cf58964bb66c7d42a202ed199a3474cb891
Jared Schneedle <jaredschneedle@Jareds-iMac.local>
2018-12-28 16:21:56 -0500

    minor changes

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Jared Schneedle on 12/25/18.
+//  Copyright © 2018 Jared Schneedle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+
+}
+
